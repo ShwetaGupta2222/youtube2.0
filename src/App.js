eb96dcaa5a2ea3e32364c19010ab65c3f68195d6
baby <210004009@hbtu.ac.in>
2023-06-18 11:11:31 +0530

    Merging

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const App = () => {
            <Navbar/>
            <Routes>
 
-            
+
+
                <Route path="/" exact element ={<Feed/>}/>
                <Route path='/video/:id' element={<VideoDetail />}/>
                <Route path="/channel/:id" element={<ChannelDetail />}/>
